9b9cc1f9bdd8f86043b747e0a29d803c4da8f0da
Sheriff Aderombi <ayaderombi@gmail.com>
2024-09-18 13:29:21 +0100

    football card project

---
+++ script.js
@@ -0,0 +1,230 @@
+const teamName = document.getElementById("team");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
+const myFavoriteFootballTeam = {
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
+  players: [
+    {
+      name: "Sergio Almirón",
+      position: "forward",
+      number: 1,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Sergio Batista",
+      position: "midfielder",
+      number: 2,
+      isCaptain: false,
+      nickname: null,
+    },
+     {
+      name: "Ricardo Bochini",
+      position: "midfielder",
+      number: 3,
+      isCaptain: false,
+      nickname: "El Bocha",
+    },
+    {
+      name: "Claudio Borghi",
+      position: "midfielder",
+      number: 4,
+      isCaptain: false,
+      nickname: "Bichi",
+    },
+    {
+      name: "José Luis Brown",
+      position: "defender",
+      number: 5,
+      isCaptain: false,
+      nickname: "Tata",
+    },
+    {
+      name: "Daniel Passarella",
+      position: "defender",
+      number: 6,
+      isCaptain: false,
+      nickname: "El Gran Capitán",
+    },
+    {
+      name: "Jorge Burruchaga",
+      position: "forward",
+      number: 7,
+      isCaptain: false,
+      nickname: "Burru",
+    },
+    {
+      name: "Néstor Clausen",
+      position: "defender",
+      number: 8,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "José Luis Cuciuffo",
+      position: "defender",
+      number: 9,
+      isCaptain: false,
+      nickname: "El Cuchu",
+    },
+    {
+      name: "Diego Maradona",
+      position: "midfielder",
+      number: 10,
+      isCaptain: true,
+      nickname: "El Pibe de Oro",
+    },
+    {
+      name: "Jorge Valdano",
+      position: "forward",
+      number: 11,
+      isCaptain: false,
+      nickname: "The Philosopher of Football",
+    },
+    {
+      name: "Héctor Enrique",
+      position: "midfielder",
+      number: 12,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Oscar Garré",
+      position: "defender",
+      number: 13,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Ricardo Giusti",
+      position: "midfielder",
+      number: 14,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Luis Islas",
+      position: "goalkeeper",
+      number: 15,
+      isCaptain: false,
+      nickname: "El loco",
+    },
+    {
+      name: "Julio Olarticoechea",
+      position: "defender",
+      number: 16,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Pedro Pasculli",
+      position: "forward",
+      number: 17,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Nery Pumpido",
+      position: "goalkeeper",
+      number: 18,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Oscar Ruggeri",
+      position: "defender",
+      number: 19,
+      isCaptain: false,
+      nickname: "El Cabezón",
+    },
+    {
+      name: "Carlos Tapia",
+      position: "midfielder",
+      number: 20,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Marcelo Trobbiani",
+      position: "midfielder",
+      number: 21,
+      isCaptain: false,
+      nickname: "Calesita",
+    },
+    {
+      name: "Héctor Zelada",
+      position: "goalkeeper",
+      number: 22,
+      isCaptain: false,
+      nickname: null,
+    },
+  ],
+};
+
+Object.freeze(myFavoriteFootballTeam); /* This object.freeze method freezes our myFavoriteFootballTeam object. The object becomes unmodifiable in any way/form. */
+const { sport, team, year, players } = myFavoriteFootballTeam; /* This is the destructuring of our object so we can use the value of its key-properties as variables without declaring a new one. */
+const { coachName } = myFavoriteFootballTeam.headCoach; /* Destructuring  the coachName property inside our headCoach nested object.  */
+
+typeOfSport.textContent = sport; /* changing the text of the typeOfSport, teamName, worldCupYear and headCoach DOM element to whatever value their destructured propery hold. */
+teamName.textContent = team; 
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => { /* This is a function for the desplay of the playerCard after they have been filtered/sorted by selected. The parameter of this function should be an array containing the player cards that match the selection criteria.*/
+  playerCards.innerHTML += arr 
+    .map( /* This is applying the map array method to the playerCards array. This will return an array with modified values of the initial array items. */
+      ({ name, position, number, isCaptain, nickname }) => /* This iterates through each array and destructures each object in it before taking an action on it.  */
+      /*  */
+        `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+      `
+    )
+    .join(""); /* Since array map method returns a new array, separated by commas, we have to remove the comma */
+};
+
+playersDropdownList.addEventListener("change", (e) => { /* This adds a change event listener to the select element. The callback function is responsible for filtering the player cards that match our selection. */
+  playerCards.innerHTML = "";
+
+  switch (e.target.value) { /* The expression we want to evaluate and run associated code on is the value of the select element which is being targeted by the event object. */
+    case "nickname": /* This checks if the value of the select element is "nickname" */
+      setPlayerCards(players.filter((player) => player.nickname !== null)); /* This code run and calls the setPlayerCards function if the case matches. The filter method is called on the players array and returns an array of items that match pass the code.   */
+      break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(
+        players.filter((player) => player.position === "midfielder")
+      );
+      break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
+    case "goalkeeper":
+      setPlayerCards(
+        players.filter((player) => player.position === "goalkeeper")
+      );
+      break;
+
+default: /* The code inside this is run if there is no case match.  */
+setPlayerCards();
+
+  }
+});
